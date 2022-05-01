b1ee2a892f54d297386d88ff1cc9d0a167695bb0
Aamnah-Khalid <aamnahkhalid3@gmail.com>
2022-05-01 12:03:45 +0500

    Show text on button click

--- Change_Text/app/src/main/java/com/aamnahkhalid/basic_code/MainActivity.java
+++ Change_Text/app/src/main/java/com/aamnahkhalid/basic_code/MainActivity.java
@@ -3,12 +3,29 @@ package com.aamnahkhalid.basic_code;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Button btn;
+    TextView txv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn= findViewById(R.id.btn);
+        TextView txv= findViewById(R.id.txt);
+
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                txv.setText("Hi Aamnah!");
+            }
+        });
     }
 }
