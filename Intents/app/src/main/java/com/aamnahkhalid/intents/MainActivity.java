e08a301e5bc433440951d25ef5d784488d29d12d
Aamnah-Khalid <aamnahkhalid3@gmail.com>
2022-05-19 19:34:29 +0500

    Intents

--- Intents/app/src/main/java/com/aamnahkhalid/intents/MainActivity.java
+++ Intents/app/src/main/java/com/aamnahkhalid/intents/MainActivity.java
@@ -1,14 +1,54 @@
 package com.aamnahkhalid.intents;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuPresenter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private MenuPresenter v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        public void onClick(View v)
+        {
+
+            Intent intent;
+
+            switch (v.getId()) {
+
+                case
+                        R.id.button:
+
+                    intent = new
+                            Intent(MainActivity.this, MainActivity2.class);
+
+                    startActivity(intent);
+
+                    break;
+
+                case R.id.button:
+
+                    Uri uri =Uri.parse("tel:+923001234567");
+
+                    intent = new
+                            Intent(Intent.ACTION_DIAL, uri);
+
+                    startActivity(intent);
+
+                    break;
+
+                default:
+
+                    throw new
+                            IllegalStateException("Unexpected value: "+ v.getId());
     }
 }
